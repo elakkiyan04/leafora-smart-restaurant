59f0dac76e823520d325347b211c6e9683119297
Elakkiyan <elakkiyan104@gmail.com>
2026-06-28 11:47:02 +0530

    Force production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,21 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
+
+const API_BASE =
+  window.location.hostname === "localhost"
+    ? "http://localhost:5000"
+    : "https://leafora-smart-restaurant.onrender.com";
 
 const api = axios.create({
-  baseURL: 'https://leafora-smart-restaurant.onrender.com',
+  baseURL: API_BASE,
 });
 
-// Request interceptor to automatically add authorization header
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('userToken');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("userToken");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
+
+  return config;
+});
 
 export default api;
